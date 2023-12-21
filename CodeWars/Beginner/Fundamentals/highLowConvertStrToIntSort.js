744b9d1ddec6049a58b2f6b428c1605714de6589
Aitilla <jhertaas@gmail.com>
2023-12-21 16:38:53 +0100

    martin

--- CodeWars/Beginner/Fundamentals/highLowConvertStrToIntSort.js
+++ CodeWars/Beginner/Fundamentals/highLowConvertStrToIntSort.js
@@ -1,5 +1,10 @@
 function highAndLow(numbers){
-    const num = numbers.split(' ').map((num) => {return parseInt(num);}).sort((numIn1, numIn2) => {return numIn1 - numIn2;});
-    return num[num.length-1] + " " + num[0];
-  }
-  
+  const num = numbers.split(' ').map((num) => {return parseInt(num);}).sort((numIn1, numIn2) => {return numIn1 - numIn2;});
+  return num[num.length-1] + " " + num[0];
+}
+
+// Martin
+function highAndLow(numbers) {
+    const arr = numbers.split(' ').map(parseInt).sort((a, b) => a-b);
+    return `${arr[arr.length-1]} ${arr[0]}`;
+}
