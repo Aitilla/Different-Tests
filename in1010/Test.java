d86159843208fba386f7affa576cbab14ea3213f
Aitilla <jhertaas@gmail.com>
2024-01-15 10:13:08 +0100

    mini push

--- in1010/Test.java
+++ in1010/Test.java
@@ -1,5 +1,5 @@
 class Test {
-    public static void main(String[] args{
+    public static void main(String[] args){
         int tall1 = 1;
         int tall2 = 2;
     
@@ -7,7 +7,7 @@ class Test {
     
         System.out.println(msg);
 
-        for (int i = i; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             System.out.println(i);
         }
 
