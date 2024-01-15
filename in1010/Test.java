f66f18f2f2ffb7c8387bf5c78d8f9b4f11cf5747
Aitilla <jhertaas@gmail.com>
2024-01-15 10:09:01 +0100

    4

---
+++ in1010/Test.java
@@ -0,0 +1,15 @@
+class Test {
+    public static void main(String[] args{
+        int tall1 = 1;
+        int tall2 = 2;
+    
+        String msg = "hallabal";
+    
+        System.out.println(msg);
+
+        for (int i = i; i < 5; i++){
+            System.out.println(i);
+        }
+
+    }
+}
