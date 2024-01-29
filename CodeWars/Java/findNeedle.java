a81e8d8adee81bd90707129c8e6ca7cddcc501cf
Joachim <jhertaas@gmail.com>
2024-01-29 14:01:34 +0100

    react

--- CodeWars/Java/findNeedle.java
+++ CodeWars/Java/findNeedle.java
@@ -1,5 +1,12 @@
 package CodeWars.Java;
 
 public class findNeedle {
-    
+    public static String needle(Object[] haystack) {
+        for (int i = 0; i < haystack.length; i++) {
+        if ("needle".equals(haystack[i])) {
+            return "found the needle at position " + i;
+        }
+        }
+        return "needle not found";
+    }
 }
